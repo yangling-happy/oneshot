cf73e16a41de4f7de5b097ed2ab18e01f9737d0f
Amber Yang <yamber0302@qq.com>
2026-04-24 21:12:20 +0800

    feat: initialize Electron Expo project

---
+++ apps/desktop/main.cjs
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false, // MVP阶段为了方便先关掉，后续可以根据安全要求开启
+    },
+  });
+
+  // 加载 Vite 开发服务的地址
+  win.loadURL("http://localhost:5173");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
